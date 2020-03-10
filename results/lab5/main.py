bda67201e247baf04ba90cd16347611ac520123a
harpsiford <kasumi2197@gmail.com>
2020-03-10 17:58:16 +0200

    Finished solution for lab5

--- results/lab5/main.py
+++ results/lab5/main.py
@@ -4,36 +4,69 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
 from inc.errorhandling import handle
 import math
 
+description = {
+  'A': 'Enter two numbers (s and t)\n', 
+  'B': 'Enter two numbers (a and n)\n'
+}
+
+# Part A
+def f(a, b, c = 1.17):
+  return (2*a - b - math.sin(c))/(5 + abs(c))
+
+# Part B
 def my_pow(a, n):
+  if a == 0:
+    raise ValueError("base number must be non-zero")
+  if n < 0:
+    raise ValueError("power must be non-negative")
   return 1 if n == 0 else a*my_pow(a, n - 1)
-  
 
-def y(a, b, c):
-  if c == 0 or a == 3:
-    return math.inf
-  return math.sin(2*a)/(a - 3) + math.atan(b)/c
+# Main
+def interpret(cmd, part):
+  try:
+    if cmd == '': return
 
-def interpret(cmd):
-  try: 
-    a, b, c = [int(x) for x in cmd.split(' ')]
-    return y(a, b, c)
+    if cmd in [':A', ':B']:
+      part = cmd[1]
+      return description[part], part
+
+    if part == 'A':
+      s, t = [float(x) for x in cmd.split(' ')]
+      return f(t, -2*s) - f(2.2, t, s - t), part
+
+    elif part == 'B':
+      a, n = [float(x) for x in cmd.split(' ')]
+      n = int (n)
+      return my_pow(a, n), part
+    
+    else:
+      raise ValueError("Part must be either A or B")
   except Exception as e:
     handle(e)
 
 def main():
-  print("Enter 3 numbers separated by spaces\n\n")
+  print("To switch between task spaces, type :A or :B\n")
+  part = 'A'
+  print(description[part])
+
   while True:
     try:
-      cmd = input("lab5> ")
-      if cmd.strip() != '':
-        result = interpret(cmd)
-        if result:
-          print(result)
-    
-    except EOFError or KeyboardInterrupt:
+      result = interpret(
+          input("lab5:{}> ".format(part)).strip(),
+          part
+        )
+      
+      if result:
+        message, part = result
+        print(message)
+
+    except KeyboardInterrupt:
       print('Bye!')
       exit(0)
     except Exception as e:
+      if e.__class__ == EOFError:
+        print('Bye!')
+        exit(0)
       handle(e)
 
 if __name__ == "__main__":
