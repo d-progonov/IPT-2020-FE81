37f522a0bd1c0b7bd44b54d5da1a73c73bdc654d
Qlorry <kostik.cu4er@gmail.com>
2020-03-14 10:39:12 +0200

    Update main.py

--- Test/main.py
+++ Test/main.py
@@ -1,20 +1,20 @@
 
-N = 3
+N = 2
 M = 4
+
 
 def countdown(N, M):
     print('Starting')
-    Temp = 0
+    Temp = 1
     Res = 1
-    while Temp < M:
+    while Temp <= M:
         yield Res
-        Res =+ pow(N, M)
-        Temp =+ 1
+        Res += pow(N, Temp)
+        Temp += 1
 
 
 val = countdown(N, M)
 
-print(next(val))
 print(next(val))
 print(next(val))
 print(next(val))
