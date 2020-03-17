ada5b24f7c8fdb64b19ecf21b7446cdeabc9156a
ViolettaKos <61617862+ViolettaKos@users.noreply.github.com>
2020-03-17 16:50:55 +0200

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,12 @@
+import math
+def function():
+    try:
+        x = int(input("Первое число:"))                
+        z = int(input("Второе число: "))
+        y = math.sqrt(abs((x**2)-4))/(2*z)
+        print("Результат:", y)
+    except ValueError:
+        print("Неправильные данные")
+    except ZeroDivisionError:
+        print("Ошибка деления на 0")  
+function()
