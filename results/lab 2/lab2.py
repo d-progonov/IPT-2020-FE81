8ccb42ab4a71e72de537ad888e69d130722277d1
Nazar Krivchuk <58174502+Scooper228@users.noreply.github.com>
2020-03-17 16:58:05 +0200

    Update lab2.py
    Rework lab2

--- results/lab 2/lab2.py
+++ results/lab 2/lab2.py
@@ -2,14 +2,9 @@ try:
     x = float(input("Enter your number: "))
     print("Enter 1 for calculating your number squared")
     print("Enter 2 to calculate  your number cubed")
+    print("and etc")
     res = int(input())
-    if res == 1:
-        x = x ** 2
-        print(x)
-    elif res == 2:
-        x = x ** 3
-        print(x)
-    else:
-        print("ERROR you should enter 1 or 2")
+    x = x ** (res+1)
+    print(x)
 except ValueError:
     print("ERROR you should enter a number but not string!!!")
