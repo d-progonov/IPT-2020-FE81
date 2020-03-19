a570cb2901c0b27d4b3d63c32f377944eb99b6d0
Santabot123 <56690519+Santabot123@users.noreply.github.com>
2020-03-19 22:28:02 +0200

    я не совсем понял что там было не правилось так что немного переделал и проверил на калькуляторе шоб всё было правильно

---
+++ results/laba3.1.py
@@ -0,0 +1,11 @@
+import math
+s=0
+#n=input("Enter n:")
+n=0
+try:
+    n=int(n)
+    while(((3**n)*math.factorial(n))/(math.factorial(3*n))>10**-4):
+        s=s+((3**n)*math.factorial(n))/(math.factorial(3*n))
+        n+=1
+    print('%.4f' % s)
+except ValueError:print("Введите другое значаение для n!!!")
