26af1848416d919618395078370240bd94fcad50
YurchenkoDenis <61078738+YurchenkoDenis@users.noreply.github.com>
2020-03-20 19:59:55 +0200

    Add files via upload

---
+++ results/Лаб2/lan_2.py
@@ -0,0 +1,11 @@
+import math
+
+while True:
+    try:
+        number = float(input("Enter number: "))
+    except ValueError:
+        print("Invalid value")
+    else:
+        break
+
+print(math.fabs(number))
