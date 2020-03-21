22994c49f584bb4ff7999c7a1673f535e919fc5e
Roman1ch <61602197+Roman1ch@users.noreply.github.com>
2020-03-21 20:45:47 +0200

    Update 3.py

--- results/3.py
+++ results/3.py
@@ -1,1 +1,11 @@
+import math
+n = int(input("Кількість елементів = : "))
+sum = 0
+i = 1
+while i <= n:
+    a = (10**i)/(math.factorial(i))
+    print(i,"- element is: ", a)
+    sum = sum + a
+    i = i + 1
+print(float("{0:.4f}".format(sum)))
 
