42fd9cf681c4e985028b90eb3fe6e4da7c785303
Roman1ch <61602197+Roman1ch@users.noreply.github.com>
2020-03-22 00:07:56 +0200

    Create dz4.py

---
+++ results/programming_beg/dz4.py
@@ -0,0 +1,8 @@
+a = int(input("a = "))
+b = int(input("b = "))
+if a > b:
+    print("Число а більше числа b ")
+elif a < b:
+    print( "Число а менше числа b")
+elif a == b:
+    print( "Ці числа рівні" )
