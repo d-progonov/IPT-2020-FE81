c74218651e02669825962e4c86938b89b1e06c16
ViolettaKos <61617862+ViolettaKos@users.noreply.github.com>
2020-03-27 14:05:56 +0200

    Add files via upload

---
+++ Lab5B.py
@@ -0,0 +1,9 @@
+n = abs(int(input("Введите натуральное число n: ")))
+m = abs(int(input("Введите натуральное число m: ")))
+
+def ncd(n, m):
+    if m == 0: 
+        return n
+    else: 
+        return ncd(m, n % m)  
+print(ncd(n,m))
