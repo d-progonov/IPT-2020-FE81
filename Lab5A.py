eb1aa2c07d620ecffbb1370a9864a66e9445a5f4
ViolettaKos <61617862+ViolettaKos@users.noreply.github.com>
2020-03-27 15:11:27 +0200

    Add files via upload

---
+++ Lab5A.py
@@ -0,0 +1,12 @@
+import math
+
+n=int(input("Кол-во элем: ")) 
+sum = 0
+for i in range(1,n+1):
+    a = math.sqrt(i)    
+    if (float.is_integer(a)):         
+        sum = sum + a
+        print(i)     
+    else:
+        print("Число не целое!")
+print(sum)
