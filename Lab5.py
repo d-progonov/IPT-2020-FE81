e7797ff30db915d30d97fd2183f2899c9021847f
kvant666ubl <61603113+kvant666ubl@users.noreply.github.com>
2020-04-01 23:42:49 +0300

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -2,10 +2,10 @@
  * File: Lab5.py
  * Project: Python Labs
  * Variant: 20
- * Created Date: 28.03.20
+ * Created Date: 28/03/2020
  * Author: kvant666ubl (GitHub)
  * -----
- * Last Modified: ***
+ * Last Modified: 01/04/2020
  * Modified By: kvant666ubl
  * -----
  * Copyright (c) Kvant Ubl 2020 
@@ -13,27 +13,28 @@
 
 # A-part
 
-def p(x, arr):
-    for elem in arr:
-        elem = elem*(x**i) 
-    return sum(arr)
+def p(x, Ar):
+    sum1 = 0
+    for i in range(0, 13):
+        elem = (Ar[i])*x**i
+        sum1 += elem
+    return sum1
+
+def polinome(A, s, t):
+    pol = p(1, A) - p(t, A) + (p(s-t, A))**2 - (p(1, A))**3 
+    print("Result of polinome is:", pol)
 
 try:
-    A = []
-    s = float(input("Ented s: "))
-    t = float(input("Ented t: "))
-    print("Add elem to a0...a12: ")
-
-    for i in range (1, 14):
-        elem = float(input())
-        A.append(elem)
+    s1 = float(input("Enter s: "))
+    t1 = float(input("Enter t: "))
     
-    res = p(1,A) - p(t, A) + (p(s-t, A))**2 - p(1, A)**3 
-    print("Result is:", res)
+    print("Enter elements from a0 to a12: ")
+    A1 = [float(input("current element: ")) for i in range(0, 13)]
 
+    polinome(A1, s1, t1)
+    
 except ValueError:
     print("Error: values must be numeric.")
-
 
 
 # B-part (4)
