2c044690f664241ca40c2e7e196093d6a1b57ac4
Qlorry <kostik.cu4er@gmail.com>
2020-04-05 12:08:55 +0300

    lab4

--- Results/Lab 4/Lab4.py
+++ Results/Lab 4/Lab4.py
@@ -1,10 +1,11 @@
 import sys, os
 import numpy as np
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from include.usefullFuncs import check_input_data
 
 
-def lab4():
+def lab4_1():
     print("HI! Lab4!")
     x = input("Input x: ")
     size = check_input_data(1, x)
@@ -13,7 +14,7 @@ def lab4():
 
     print(A)
 
-    middle = int(size/2)
+    middle = int(size / 2)
     leftTop = A[:middle, :middle]
     rightTop = A[:middle, middle:size]
     leftBottom = A[middle:size, :middle]
@@ -28,4 +29,39 @@ def lab4():
     print(B)
 
 
-print(lab4())
+class Circle:
+
+    def __init__(self, foo):
+        """Constructor"""
+        self.foo = foo
+        self.size = len(foo)
+
+    def get(self, n):
+        num = n % self.size
+        return self.foo[num]
+
+    def remove(self, n):
+        num = n % self.size
+        print("removing ", self.foo[num])
+        del self.foo[num]
+        self.size = self.size - 1
+
+
+def lab4_2():
+    foo = [0, 1, 2, 3, 4, 5, 6, 7]
+
+    circle = Circle(foo)
+
+    #print(circle.get(10))
+
+    for i in range(len(foo)+10, 10, -1):
+        print(circle.get(i))
+
+    circle.remove(7)
+    circle.remove(7)
+
+    for i in range(10, len(foo)+10, 1):
+        print(circle.get(i))
+
+# print(lab4_1())
+print(lab4_2())
