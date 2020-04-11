d4f63f3c63cb07007c17d5089d60fb9a7ad3cf74
danny-mellen <61392457+danny-mellen@users.noreply.github.com>
2020-04-11 09:26:43 +0300

    Add files via upload

---
+++ Task_11.04/Task_11.04_pr.py
@@ -0,0 +1,17 @@
+from tkinter import *
+ 
+root = Tk()
+root.title("Графическая программа на Python")
+root.geometry("400x300")
+ 
+btn1 = Button(text="Start")
+
+btn2 = Button(text="Check")
+
+btn3 = Button(text="Finish")
+
+btn1.pack()
+btn3.pack()
+btn2.pack()
+
+root.mainloop()
