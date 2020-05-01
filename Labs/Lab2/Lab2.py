af82990171b3a9b2995a7529fc7e613f9b556ec9
ViolettaKos <61617862+ViolettaKos@users.noreply.github.com>
2020-05-01 23:13:41 +0300

    Add files via upload

---
+++ Labs/Lab2/Lab2.py
@@ -0,0 +1,13 @@
+try:
+    x=int(input("Введите число:"))
+    res=abs(x)
+    print("Абсолютное значение числа:", res)
+
+except ValueError:
+        print("Неправильные данные")
+
+
+
+
+
+        
