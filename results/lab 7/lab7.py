bea038d3044356940c768e7a21c0388df0af3fc9
Nazar Krivchuk <58174502+Scooper228@users.noreply.github.com>
2020-05-01 14:24:16 +0300

    Update lab 7.py

--- results/lab 7/lab7.py
+++ results/lab 7/lab7.py
@@ -1,20 +1,28 @@
 n = int(input("Enter n:"))
-cup=[]
+cup = []
 lis = []
 for i in range(0,20):
     cup.append(0)
-for i in range (0,n) :
-    s = input("Enter word:")
-    if s.__len__() > 20:
-        s = input("Enter word again length should be less than 20!:")
-    lis.insert(i, s)
-for i in range (len(lis)):
-    for j in range(0,20):
+
+for i in range(0, n) :
+    s = input("Enter your words:")
+    temp = s.split()
+
+    for k in range(len(temp)):
+        if len(temp[k]) > 20:
+            s = input("Enter word again length should be less than 20!:")
+
+    lis.extend(temp)
+
+for i in range(len(lis)):
+    for j in range(0, 20):
         if len(lis[i]) == j:
             cup[j] += 1
+
 for i in range(len(cup)):
     if cup[i] != 0:
         print(i, " symbol = ", cup[i])
+
 print("Word count is :", len(lis))
 
 
