bef8baa4e50f4631c2b403fa059a769e7f371188
Artem Makartsov <kingston212mak@gmail.com>
2020-05-07 11:30:54 +0300

    lab10

--- result/lab10/lab10.py
+++ result/lab10/lab10.py
@@ -1,5 +1,3 @@
-import math
-
 def get_input():
     inpt = input('Write number to check or Enter to exit:\n')
     if len(inpt) == 0:
@@ -7,7 +5,7 @@ def get_input():
     else:
         try:
             num = int(inpt)
-            if num < 1 or num > 1e+6:
+            if num < 1 or num >= 1e+12:
                 raise ValueError
             return num
         except ValueError:
@@ -15,29 +13,42 @@ def get_input():
             return False
 
 
+class PrimesFinder:
+    def __init__(self):
+        self.initial_primes_list = self.create_primes_list(int(1e+3), list(range(2, int(1e+6))))
 
-def create_primes_list(num, lst, pos = 0):
-    if lst[pos] > math.sqrt(num) + 1:
-        return lst
-    else:
-        lst = list(n for n in lst if (n % lst[pos] != 0 or n == lst[pos]))
-        pos += 1
-        return create_primes_list(num,lst,pos)
+    def create_primes_list(self, sqrt_num_, lst, pos = 0):
+        if lst[pos] > sqrt_num_:
+            return lst
+        else:
+            lst = list(x for x in lst if (x % lst[pos] != 0 or x == lst[pos]))
+            return self.create_primes_list(sqrt_num_, lst, pos + 1)
+
+    def is_prime(self, num):
+        if num < 1e+6:
+            return (num in self.initial_primes_list)
+
+        check_to = num ** (1/2)
+        for i in range(0, len(self.initial_primes_list)):
+            if self.initial_primes_list[i] > check_to:
+                return True
+            if num % self.initial_primes_list[i] == 0 and num != self.initial_primes_list[i]:
+                return False
+        return -1
+
 
 def main():
-    primes_list = list()
-    to_num = 0
+    print('Initialization...')
+    f = PrimesFinder()
     num = get_input()
     while True:
         if bool(num):
-            if num > to_num:
-                if num > 4e+5:
-                    print('Computing....')
-                primes_list = create_primes_list(num,list(range(2, num + 1)) )
-                to_num = num
-            if num in primes_list:
-                print('{} IS prime'.format(num))
+            is_p = f.is_prime(num)
+            if is_p:
+                print('Number {} IS prime'.format(num))
             else:
-                print('{} is NOT prime'.format(num))
+                print('Number {} is NOT prime'.format(num))
         num = get_input()
+
+
 main()
