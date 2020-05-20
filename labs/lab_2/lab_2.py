4453807a98cd810c9a2928798aa0fbd99aeb0a28
MadVA <35272939+MadVA@users.noreply.github.com>
2020-05-20 20:49:12 +0300

    Add files via upload

---
+++ labs/lab_2/lab_2.py
@@ -0,0 +1,35 @@
+def exit_func():
+   e = input("If you want to end this program, please input \"e\": ")
+   if(e.lower() == 'e'):
+      raise SystemExit(0)
+
+def comparison():
+    try:
+        x = float(input("Please, input 1-st number: "))
+        z = float(input("Please, input 2-nd number: "))
+        y = float(input("Please, input 3-d number: "))
+        if x < z :
+            if x == y:
+                print("Minimum is x which equal to y:", x)
+            elif x < y:
+                print("Minimum x:", x)
+        elif y < x:
+            if y == z:
+                print("Minimum is y which is equal to z:", y)
+            elif y < z:
+                print("Minimum is y:", y)
+        elif z < x:
+            if z == y:
+                print("Minimum is z which is equal to y:", y)
+            elif z < y:
+                print("Minimum is z:", z)
+        else:
+            print("All numbers are equal.")
+
+    except ValueError:
+        print("\nYou have to enter only a number. Please, repeat your input.")
+        exit_func()
+        comparison()
+
+if __name__ == "__main__":
+    comparison()
