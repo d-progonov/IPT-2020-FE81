756879f282278efb639b209a3ed07057ee20a441
valeriikocherha <48161975+valeriikocherha@users.noreply.github.com>
2020-05-20 09:53:18 +0300

    Add files via upload

--- results/laba 10/lab_10.py
+++ results/laba 10/lab_10.py
@@ -1,27 +1,65 @@
-import numpy as np
+import math
+import operator
+from functools import reduce
+
+
+def check_help(NUM):
+    check_help = True
+    try:
+        num_1 = int(NUM)
+    except Exception:
+        print("Ошибка! Уведите целое число")
+        check_help = False
+    return check_help
 
 
 def check(NUM):
     check_help = True
     try:
         num_1 = float(NUM)
-    except ValueError:
-        print("Ошибка, уведите целое или дробное число")
+    except Exception:
+        print("Ошибка! Уведите целое или дробное число")
         check_help = False
     return check_help
 
 
-def square(x1, x2, y1, y2):
-    res_1 = abs(float((x1 * y2 - x2 * y1) / 2))
-    return res_1
+def square(mass):
+    n = len(mass)
+    S = 0
+    for i in range(0, n):
+        x1 = mass[i][0]
+        y1 = mass[i][1]
+        x2 = mass[(i + 1) % n][0]
+        y2 = mass[(i + 1) % n][1]
+        S += x1 * y2 - x2 * y1
+    res = abs(S / 2)
+    return res
+
+
+def func(a, b, c):
+    ch = False
+    if ((b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0])) < 0:
+        ch = True
+    if ((b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0])) > 0:
+        ch = False
+    return ch
+
+
+def scan(mass):
+    S = [mass[0], mass[1]]
+    for i in range(2, len(mass)):
+        while func(S[-2], S[-1], mass[i]) == True:
+            del S[-1]
+        S.append(mass[i])
+    return S
 
 
 def work():
     while 1:
         n = input("Уведите количество точек:  ")
-        if check(n) == False:
+        if check_help(n) == False:
             continue
-        if int(n) <= 1:
+        if int(n) <= 0:
             print("Ошибка, невозможно такое количество точек")
             continue
         else:
@@ -38,30 +76,26 @@ def work():
             else:
                 num_x = float(num_x)
                 num_y = float(num_y)
-                mass.append((num_x, num_y))
+                mass.append([num_x, num_y])
                 n -= 1
         print(mass)
-        list.sort(mass)
-        print(mass)
-        x = []
-        y = []
-        for i in mass:
-            x.append(i[0])
-            y.append(i[1])
-        print(x)
-        print(y)
-
-        k = np.size(x)
-        res = 0
-        for i in range(0, k, 1):
-            if i == (k - 1):
-                z.append(square(x[0], x[k - 1], y[1], y[k - 1]))
-            else:
-                z.append(square(x[i], x[i + 1], y[i], y[i + 1]))
-            res += z[i]
-        print("Площадь н-угольника ровняется: " + str(res))
-
-        break
+        # center = tuple(map(operator.truediv, reduce(lambda x, y: map(operator.add, x, y), mass), [len(mass)] * 2))
+        center = min(mass)
+        center = [center[0] - 1, center[1] + 0.11]
+        print(center)
+        mass_2 = sorted(mass, key=lambda coord: (-135 - math.degrees(
+            math.atan2(*tuple(map(operator.sub, coord, center))[::1]))) % 360)
+        print(mass_2)
+        if len(mass_2) == 1:
+            S = 0
+            print("Площадь многоугольника:", S)
+            break
+        else:
+            mass_3 = scan(mass_2)
+            print(mass_3)
+            S = square(mass_3)
+            print("Площадь многоугольника:", S)
+            break
 
 
 if __name__ == "__main__":
